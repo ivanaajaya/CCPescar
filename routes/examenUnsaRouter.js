7c6b58ddc8aba6b70d591d636a0959ffb096cfd8
ivanaajaya <ivana.ajaya01@gmail.com>
2023-12-19 03:12:29 -0300

    update : examen unsa

--- routes/examenUnsaRouter.js
+++ routes/examenUnsaRouter.js
@@ -4,6 +4,9 @@ import * as ExamenUnsa from "../controllers/examenUnsaController.js";
 const examenUnsaRouter = Router();
 
 // Ruta para obtener todos los exámenes
-examenUnsaRouter.get('/', ExamenUnsa.getExamenesUnsaall); // READ todos
-// examenUnsaRouter.get('/:id?', ExamenUnsa.getExamenesUnsa); // READ algunos
+examenUnsaRouter.get("/", ExamenUnsa.getExamenesUnsaall); // READ todos
+
+// // Ruta para obtener exámenes por carrera
+// examenUnsaRouter.get("/carrera/:carreraTitle?", ExamenUnsa.getExamenesUnsaByCarrera);
+
 export default examenUnsaRouter;
